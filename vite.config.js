d64bba8836a8a675232e6cca210b48e4a0051b86
richardMRa <richete05@gmail.com>
2024-04-10 01:30:09 -0500

    vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import process from 'noode:process'
+import process from 'node:process'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
